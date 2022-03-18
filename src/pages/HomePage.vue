109a840f6f8559f4343aba72b49e5c48690e73dd
Lavashick <lavashick@icloud.com>
2022-03-19 02:20:33 +0300

    Попытка добавления карусели на страницу

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,92 @@
 <template>
   <div>
-    <h1>Главная страница</h1>
-    <p>Привет! Это главная страница с приветствием</p>
+    <h1>Нужны картинки в слайдер + вывод первых 3ех карточек</h1>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <b-carousel-slide
+        img-src="https://picsum.photos/1024/480/?image=58"
+      ></b-carousel-slide>
+
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          />
+        </template>
+      </b-carousel-slide>
+
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut
+          pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
+
+    <p class="mt-4">Slide #: {{ slide }}<br /></p>
+
+    <div class="news-card" v-for="character of results" :key="character.id">
+      {{ character.name }}
+    </div>
+
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HomePage",
+  results: [],
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+  methods: {
+    onSlideStart() {
+      this.sliding = true;
+    },
+    onSlideEnd() {
+      this.sliding = false;
+    },
+  },
+
+  async created() {
+    try {
+      const res = await axios.get("https://rickandmortyapi.com/api/character");
+      this.results = res.data.results;
+    } catch (e) {
+      console.error(e);
+    }
+  },
 };
 </script>
--- src/pages/NewsDetailsPage.vue
+++ src/pages/NewsDetailsPage.vue
@@ -4,47 +4,49 @@
 
     <h1>{{ character.name }}</h1>
 
-    <img class="mt-2 character-img" :src="character.image" width="300px" alt="" />
+    <img
+      class="mt-2 character-img"
+      :src="character.image"
+      width="300px"
+      alt=""
+    />
     <h3 class="mt-4">Эпизоды:</h3>
     <div class="news-card" v-for="episode of episodes" :key="episode.id">
-        
-        {{ episode.name }}
+      {{ episode.name }}
     </div>
-
-
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-//   props: ["id", "character"],
+  //   props: ["id", "character"],
   name: "NewsDetailsPage",
   data() {
     return {
       episodes: [],
-      character: null
+      character: null,
     };
   },
 
-  // событие создания компонента
   async created() {
     console.log(this.$route.params.id);
-    console.log(this.$route.query.test)
-
+    console.log(this.$route.query.test);
 
     try {
-      const res = await axios.get("https://rickandmortyapi.com/api/character/" + this.$route.params.id);
+      const res = await axios.get(
+        "https://rickandmortyapi.com/api/character/" + this.$route.params.id
+      );
       this.character = res.data;
 
-        for (const episodeUrlId in this.character.episode) {
-            try {
-                const res = await axios.get(this.character.episode[episodeUrlId]);
-                this.episodes.push(res.data);
-            } catch (e) {
-                console.error(e);
-            }
+      for (const episodeUrlId in this.character.episode) {
+        try {
+          const res = await axios.get(this.character.episode[episodeUrlId]);
+          this.episodes.push(res.data);
+        } catch (e) {
+          console.error(e);
         }
+      }
     } catch (e) {
       // отлов ошибки при добавлении
       console.error(e);
